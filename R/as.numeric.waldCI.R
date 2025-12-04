a0bf01354a724bbd8ebffa2ebf73a9b7a6077ff5
Josh Errickson <jerrick@umich.edu>
2025-12-04 10:46:42 -0500

    Fix exports to satisfy document()

--- R/as.numeric.waldCI.R
+++ R/as.numeric.waldCI.R
@@ -4,9 +4,9 @@ NULL
 ##' Convert waldCI to numeric
 ##' @param x waldCI object
 ##' @return Length 2 vector of the bounds of `x`.
-##' @export
 setGeneric("as.numeric")
 
+##' @export
 setMethod("as.numeric", "waldCI", function(x) {
   return(.getBounds(x))
 })
